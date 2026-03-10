14b7b6d7480c974835d522d2770920dc23100681
AayushSharma119 <aayush.sharma1109@gmail.com>
2026-03-11 01:19:11 +0530

    Connected Flask app to MySQL database

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# MySQL Database Connection
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:golu1109@localhost/fitness_buddy'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
