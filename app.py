c921b28b2236c3c33c60a28257bade2335788ee4
AayushSharma119 <aayush.sharma1109@gmail.com>
2026-03-11 00:52:58 +0530

    Added home page template

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Fitness Tracker Website Running!</h1>"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
